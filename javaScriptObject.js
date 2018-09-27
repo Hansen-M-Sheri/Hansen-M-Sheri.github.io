79f9ca86a10e52719b05ae779f9d9fc8c3112ef5
Sheri <10212888+Hansen-M-Sheri@users.noreply.github.com>
2018-09-27 15:06:11 -0600

    fix hopefully working tryAgain

--- javaScriptObject.js
+++ javaScriptObject.js
@@ -1,9 +1,5 @@
 <!--hide script from old browsers
 
-function myFunction(randomString) { 
-    document.getElementById("demo").innerHTML = randomItem;
-}
-
 function magicAnswers() {
 	var answers = ["Yes", "No", "Definitely!", "Absolutely Not!", "In your dreams", "You wish!", 
 	"It is certain", "You may rely on it", "Cannot predict now", "Concentrate and ask again", "Very doubtful",
