09d6bf9ea4c31180e86422c3402ade185b921fe6
Sheri <10212888+Hansen-M-Sheri@users.noreply.github.com>
2018-09-28 08:16:28 -0600

    inheritance

--- javaScriptObjects.js
+++ javaScriptObjects.js
@@ -23,12 +23,19 @@ User.prototype.logout = function() {
 	console.log(this.email, 'has logged out');
 }
 
+//Admin is a User - requires inheritance
+function Admin(...args) { //this takes all parameters and puts then in an array
+	//pass in the new Admin we created on line 36 by passing in "this"
+  User.apply(this, [args]); //apply will run the function called, which in this case is User 
+  							//(the 2nd param requires an array)
+}
+
 //Every time this constructor is used, it will have access to login function
 
 var userOne = new User ('user1@email.com', 'Sheri');
 var userTwo = new User ('user2@email.com', 'User2');
+var admin = new Admin ('admin@email', 'Jacob');
 
-console.log(userOne);
-userTwo.login();
+console.log(admin);
 
 
