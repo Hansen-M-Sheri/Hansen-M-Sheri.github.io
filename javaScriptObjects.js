3b7927e29d4149cf592bf4a2bc0112b7322e3e3e
Sheri <10212888+Hansen-M-Sheri@users.noreply.github.com>
2018-09-28 08:17:49 -0600

    inheritance method added

--- javaScriptObjects.js
+++ javaScriptObjects.js
@@ -28,6 +28,9 @@ function Admin(...args) { //this takes all parameters and puts then in an array
 	//pass in the new Admin we created on line 36 by passing in "this"
   User.apply(this, [args]); //apply will run the function called, which in this case is User 
   							//(the 2nd param requires an array)
+
+  //can add properties or methods ONLY accessible to admin here:
+  this.role = 'super admin';
 }
 
 //Every time this constructor is used, it will have access to login function
