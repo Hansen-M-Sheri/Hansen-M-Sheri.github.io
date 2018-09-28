7f8cbc750edb51b32946e95a1434e1b4e4f7ada2
Sheri <10212888+Hansen-M-Sheri@users.noreply.github.com>
2018-09-28 08:04:01 -0600

    Add method to prototype

--- javaScriptObjects.js
+++ javaScriptObjects.js
@@ -4,10 +4,23 @@ function User(email, name) {
 	this.email = email;
 	this.name = name;
 	this.online = false;
+	/*
 	this.login = function() {
 		this.online = true;
 		console.log(this.email, 'has logged in');
 	}
+	*/
+}
+
+//Add a method to the prototype
+User.prototype.login = function() {
+	this.online = true;
+		console.log(this.email, 'has logged in');
+}
+
+User.prototype.logout = function() {
+	this.online = false;
+	console.log(this.email, 'has logged out');
 }
 
 //Every time this constructor is used, it will have access to login function
@@ -17,3 +30,5 @@ var userTwo = new User ('user2@email.com', 'User2');
 
 console.log(userOne);
 userTwo.login();
+
+
