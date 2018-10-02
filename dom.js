21b46893683429687f87f19f91fdadfb7b350721
Sheri <10212888+Hansen-M-Sheri@users.noreply.github.com>
2018-10-02 12:17:50 -0600

    fix  for submit null

--- dom.js
+++ dom.js
@@ -1,10 +1,10 @@
 
-var form = document.getElementById('form');
+var form = document.getElementById("form");
 //put UL in variable
 var itemList = document.getElementById("items");
 
 //form submit event
-form.addEventListener('submit', addItem());
+form.addEventListener("submit", addItem());
 
 function addItem(e) { //pass in event object
 	e.preventDefault();
