2b1cf31f0574f0cfd8fdc765468b9294954b7015
Sheri <10212888+Hansen-M-Sheri@users.noreply.github.com>
2018-10-02 20:32:15 -0600

    create new li and delete button for li items

--- dom.js
+++ dom.js
@@ -9,7 +9,27 @@ form.addEventListener('submit', addItem());
 
 function addItem(e) { //pass in event object
 	//e.preventDefault();
-	console.log(1);
-	
+	//console.log(1);
+	var newItem = document.getElementById("newItem").value;
+
+	//create a new li element
+	var li = document.createElement('li');
+
+	//append the text from the form to the new list item
+	li.appendChild(document.createTextNode(newItem));
+
+	//create a delete button element
+	var deleteButton = document.createElement('button');
+
+	//add class to button
+	deleteButton.className = 'deleteButton';
+
+	//append text node
+	deleteButton.appendChild(document.createTextNode('X'));
+
+	//append button to li
+	li.appendChild(deleteButton);
+
+	itemList.appendChild(li);
 
 }
