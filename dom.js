c82def06b7c40ee38fbbd60a6f3b6c8cfd6cacd1
Sheri <10212888+Hansen-M-Sheri@users.noreply.github.com>
2018-10-02 20:35:10 -0600

    move all script within function and add onload to execute

--- dom.js
+++ dom.js
@@ -1,35 +1,37 @@
 
-var form = document.getElementById("form");
-console.log(form);
-//put UL in variable
-var itemList = document.getElementById("items");
+function createListItems() {
+	var form = document.getElementById("form");
+	console.log(form);
+	//put UL in variable
+	var itemList = document.getElementById("items");
 
-//form submit event
-form.addEventListener('submit', addItem());
+	//form submit event
+	form.addEventListener('submit', addItem());
 
-function addItem(e) { //pass in event object
-	//e.preventDefault();
-	//console.log(1);
-	var newItem = document.getElementById("newItem").value;
+	function addItem(e) { //pass in event object
+		//e.preventDefault();
+		//console.log(1);
+		var newItem = document.getElementById("newItem").value;
 
-	//create a new li element
-	var li = document.createElement('li');
+		//create a new li element
+		var li = document.createElement('li');
 
-	//append the text from the form to the new list item
-	li.appendChild(document.createTextNode(newItem));
+		//append the text from the form to the new list item
+		li.appendChild(document.createTextNode(newItem));
 
-	//create a delete button element
-	var deleteButton = document.createElement('button');
+		//create a delete button element
+		var deleteButton = document.createElement('button');
 
-	//add class to button
-	deleteButton.className = 'deleteButton';
+		//add class to button
+		deleteButton.className = 'deleteButton';
 
-	//append text node
-	deleteButton.appendChild(document.createTextNode('X'));
+		//append text node
+		deleteButton.appendChild(document.createTextNode('X'));
 
-	//append button to li
-	li.appendChild(deleteButton);
+		//append button to li
+		li.appendChild(deleteButton);
 
-	itemList.appendChild(li);
+		itemList.appendChild(li);
 
+	}
 }
