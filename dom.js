994dfcb97b4bb87a4f33c8915a35b6e9654f8a0c
Sheri <10212888+Hansen-M-Sheri@users.noreply.github.com>
2018-10-02 21:22:43 -0600

    add full script to dom.js not in head

--- dom.js
+++ dom.js
@@ -1,37 +1,52 @@
 
 function createListItems() {
-	var form = document.getElementById("form");
-	console.log(form);
-	//put UL in variable
-	var itemList = document.getElementById("items");
+		var form = document.getElementById("form");
+		//put UL in variable
+		var itemList = document.getElementById("items");
 
-	//form submit event
-	form.addEventListener('submit', addItem());
+		//delete event
+		itemList.addEventListener('click', removeItem);
+	    //form submit event
+		form.addEventListener('submit', addItem());
+	}
 
 	function addItem(e) { //pass in event object
-		//e.preventDefault();
-		//console.log(1);
-		var newItem = document.getElementById("newItem").value;
+	var itemList = document.getElementById("items");
+	var newItem = document.getElementById("newItem").value;
 
-		//create a new li element
-		var li = document.createElement('li');
+	//create a new li element
+	var li = document.createElement('li');
 
-		//append the text from the form to the new list item
-		li.appendChild(document.createTextNode(newItem));
+	//append the text from the form to the new list item
+	li.appendChild(document.createTextNode(newItem));
 
-		//create a delete button element
-		var deleteButton = document.createElement('button');
+	//create a delete button element
+	var deleteButton = document.createElement('button');
 
-		//add class to button
-		deleteButton.className = 'deleteButton';
+	//add class to button
+	deleteButton.className = 'deleteButton';
 
-		//append text node
-		deleteButton.appendChild(document.createTextNode('X'));
+	//append text node
+	deleteButton.appendChild(document.createTextNode('X'));
 
-		//append button to li
-		li.appendChild(deleteButton);
+	//append button to li
+	li.appendChild(deleteButton);
 
-		itemList.appendChild(li);
-
+	itemList.appendChild(li);
 	}
-}
+	
+
+
+	function removeItem(e) {
+		//only continue if the target clicked on is in class //deleteButton
+		if(e.target.classList.contains('deleteButton')) {
+			if(confirm('Are you sure?')) {
+				//get the parentElement (li)
+				var li = e.target.parentElement;
+				//remove the ul from itemlist
+				var itemList = document.getElementById("items");
+				itemList.removeChild(li);
+
+			}
+		}
+	}
