01092e867056b774b468a1bb8700058a9bd5b856
Sheri Hansen <10212888+Hansen-M-Sheri@users.noreply.github.com>
2018-10-24 10:59:11 -0600

    fix syntax transform rotate

--- css3.js
+++ css3.js
@@ -1,13 +1,15 @@
 function castSpell() {
-	//rotate wand 2 times to left and then 4 times to right to simulate shaking
+	//transform wand 2 times to left and then 4 times to right to simulate shaking
+	//var wand = document.getElementById("wand");
 	var wand = document.getElementById("wand");
-	wand.style.rotate = -10;
-	wand.style.rotate = -10;
-	wand.style.rotate = 10;
-	wand.style.rotate = 10;
-	wand.style.rotate = 10;
-	wand.style.rotate = 10;
-	wand.style.rotate = -10;
-	wand.style.rotate = -10;
+
+	wand.style.transform = "rotate(-10deg)";
+	wand.style.transform = "rotate(-10deg)";
+	wand.style.transform = "rotate(10deg)";
+	wand.style.transform = "rotate(10deg)";
+	wand.style.transform = "rotate(10deg)";
+	wand.style.transform = "rotate(10deg)";
+	wand.style.transform = "rotate(-10deg)";
+	wand.style.transform = "rotate(-10deg)";
 
 }
