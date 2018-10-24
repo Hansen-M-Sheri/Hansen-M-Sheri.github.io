ff96fb39c118c992557f48183882ccfa2deb0aff
Sheri Hansen <10212888+Hansen-M-Sheri@users.noreply.github.com>
2018-10-24 10:38:13 -0600

    fix style rotate

--- css3.js
+++ css3.js
@@ -1,13 +1,13 @@
 function castSpell() {
 	//rotate wand 2 times to left and then 4 times to right to simulate shaking
 	var wand = getElementById("wand");
-	wand.style.rotate(-10);
-	wand.style.rotate(-10);
-	wand.style.rotate(10);
-	wand.style.rotate(10);
-	wand.style.rotate(10);
-	wand.style.rotate(10);
-	wand.style.rotate(-10);
-	wand.style.rotate(-10);
+	wand.style.rotate = -10;
+	wand.style.rotate = -10;
+	wand.style.rotate = 10;
+	wand.style.rotate = 10;
+	wand.style.rotate = 10;
+	wand.style.rotate = 10;
+	wand.style.rotate = -10;
+	wand.style.rotate = -10;
 
 }
