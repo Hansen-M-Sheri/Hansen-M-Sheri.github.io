dc92986ef33d481550593a3873dfbf3da188ccea
Sheri Hansen <10212888+Hansen-M-Sheri@users.noreply.github.com>
2018-10-24 11:34:31 -0600

    setsSpellName

--- css3.js
+++ css3.js
@@ -1,4 +1,4 @@
-function castSpell() {
+function castSpell(spellName) {
 	//transform wand 2 times to left and then 4 times to right to simulate shaking
 	//var wand = document.getElementById("wand");
 	var wand = document.getElementById("wand");
@@ -14,4 +14,16 @@ function castSpell() {
 	// wand.style.transform = "rotate(-10deg)";
 	// wand.style.transform = "rotate(-10deg)";
 
+}
+function setScale() {
+document.getElementByID("spellName").value = "rotate";
+}
+function setShrink() {
+document.getElementByID("spellName").value = "shrink";
+}
+function setEnlarge() {
+document.getElementByID("spellName").value = "enlarge";
+}
+function setTranslate() {
+document.getElementByID("spellName").value = "translate";
 }
