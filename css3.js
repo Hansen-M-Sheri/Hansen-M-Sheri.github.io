e68730b0ad8f508bd60efbdc4640b6151846e001
Sheri Hansen <10212888+Hansen-M-Sheri@users.noreply.github.com>
2018-10-24 11:01:44 -0600

    add console.log for testing

--- css3.js
+++ css3.js
@@ -2,8 +2,10 @@ function castSpell() {
 	//transform wand 2 times to left and then 4 times to right to simulate shaking
 	//var wand = document.getElementById("wand");
 	var wand = document.getElementById("wand");
+	console.log("onclick worked");
 
 	wand.style.transform = "rotate(-10deg)";
+	console.log("first transform complete");
 	wand.style.transform = "rotate(-10deg)";
 	wand.style.transform = "rotate(10deg)";
 	wand.style.transform = "rotate(10deg)";
