957df4823feee7be6e8f3ccd012a09a284969f15
Sheri Hansen <10212888+Hansen-M-Sheri@users.noreply.github.com>
2018-11-07 13:38:44 -0700

    drawing

--- canvas.js
+++ canvas.js
@@ -5,4 +5,20 @@ canvas.height = window.innerHeight;
 
 //draw on the canvas c = context
 var c = canvas.getContext('2d');
+c.fillStyle="green";
 c.fillRect(100, 100, 30, 30);
+
+//line
+c.beginPath();
+c.moveTo(50, 300);
+c.lineTo(300, 100);
+c.strokeStyle = "red";
+c.stroke();
+
+//arc / circle
+c.beginPath();
+c.arc(300, 300, 30, 0, Math.PI * 2, false);
+c.beginPath();
+c.arc(300, 300, 20, 0, Math.PI * 2, false);
+c.strokeStyle = "blue";
+c.stroke();
