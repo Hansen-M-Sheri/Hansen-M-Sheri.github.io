540c75ca6a8d5df9767ddd36ecf89e8183a69156
Sheri Hansen <10212888+Hansen-M-Sheri@users.noreply.github.com>
2018-11-07 20:18:28 -0700

    test canvas animate

--- canvas.js
+++ canvas.js
@@ -3,43 +3,52 @@ var canvas = document.querySelector('canvas');
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
-//draw on the canvas c = context
-var c = canvas.getContext('2d');
-c.fillStyle="green";
-c.fillRect(100, 100, 30, 30);
+// //draw on the canvas c = context
+// var c = canvas.getContext('2d');
+// c.fillStyle="green";
+// c.fillRect(100, 100, 30, 30);
 
-//line
-c.beginPath();
-c.moveTo(50, 300);
-c.lineTo(300, 100);
-c.strokeStyle = "red";
-c.stroke();
+// //line
+// c.beginPath();
+// c.moveTo(50, 300);
+// c.lineTo(300, 100);
+// c.strokeStyle = "red";
+// c.stroke();
 
-//arc / circle
-c.beginPath();
-c.arc(300, 300, 30, 0, Math.PI * 2, false);
-c.beginPath();
-c.arc(300, 300, 20, 0, Math.PI * 2, false);
-c.strokeStyle = "blue";
-c.stroke();
+// //arc / circle
+// c.beginPath();
+// c.arc(300, 300, 30, 0, Math.PI * 2, false);
+// c.beginPath();
+// c.arc(300, 300, 20, 0, Math.PI * 2, false);
+// c.strokeStyle = "blue";
+// c.stroke();
 
-//create a lot of circles with a forloop
-
-var x = Math.random() * window.innerWidth;
-var y = Math.random() * window.innerHeight;
+// //create a lot of circles with a forloop
+// for (var i = 0; i < 20; i++) {
+// 	var x = Math.random() * window.innerWidth;
+// 	var y = Math.random() * window.innerHeight;
+// 	c.beginPath();
+// 	c.arc(300, 300, 30, 0, Math.PI * 2, false);
+	
+// 	c.strokeStyle = "blue";
+// 	c.stroke();
+// }
+var x = Math.random() * innerWidth;
+var y = Math.random() * innerHeight;
+var dx = (Math.random() - 0.5) * 8;
+var dy = (Math.random() - 0.5) * 8;
+var radius = 30;
 function animate() {
 	requestAnimationFrame(animate);
 	c.clearRec(0,0,innerWidth, innerHeight);
 	c.beginPath();
-
-	for (var i = 0; i < 20; i++) {
-	
-	c.beginPath();
-	c.arc(300, 300, 30, 0, Math.PI * 2, false);
+	c.arc(x, y, radius, 0, Math.PI * 2, false);
 	
 	c.strokeStyle = "blue";
 	c.stroke();
-}
-	x += 1;
-	y -= 1;
+
+	if(x + radius>innerWidth || x - radius < 0) {
+		dx = -dx;
+	}
+	x += dx;
 }
