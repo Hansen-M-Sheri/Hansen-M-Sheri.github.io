e46e6cef7f06e52d924e46af89f367c8c90795d5
Sheri Hansen <10212888+Hansen-M-Sheri@users.noreply.github.com>
2018-11-07 22:25:10 -0700

    animate

--- canvas.js
+++ canvas.js
@@ -36,21 +36,28 @@ var c = canvas.getContext('2d');
 // 
 
 var x = 200;
+var y = 200;
 var dx = 1;
+var dy = 4;
 var radius = 30;
 function animate() {
 	requestAnimationFrame(animate);
 	c.clearRect(0,0, innerWidth, innerHeight);	
 
 	c.beginPath();
-	c.arc(x,200, radius, 0, Math.PI * 2, false);
+	c.arc(x, y, radius, 0, Math.PI * 2, false);
 	c.strokeStyle = "blue";
 	c.stroke();
 
-	if(x + radius > innerWidth) {
+	if(x + radius > innerWidth || x - radius < 0) {
 		dx = -dx;
 	}
+
+	if(y + radius > innerWidth || y - radius < 0) {
+		dy = -dy;
+	}
 	x += dx;
+	y += dy;
 }
 
 animate();
