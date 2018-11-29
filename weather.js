363f228db883e9fe08ad711fcdd94c8b19c43a92
Sheri Hansen <10212888+Hansen-M-Sheri@users.noreply.github.com>
2018-11-28 22:35:49 -0700

    get id to select background

--- weather.js
+++ weather.js
@@ -1,17 +1,14 @@
-//Weather object constructor
-//  Weather() {
-// 	this.
-// }
-
+function Weather(id, image){
+	this.id = id;
+	this.image = image;
+}
 
 /*
 * Call Weather API
 * use ajax to get the weather for the current zip code
 */
-function callWeatherApi() {
+function callWeatherApi(city, country) {
 	var xmlhttp = new XMLHttpRequest();
-	var city = "Boise";
-	var country = "us";
 	var api_key = "94c477428543f4d7a37d952a58632aab";
 	var appID = "&APPID=" + api_key;
 	var url = "https://api.openweathermap.org/data/2.5/weather?q=";
@@ -20,8 +17,7 @@ function callWeatherApi() {
 	xmlhttp.onreadystatechange = function() {
 		if (this.readyState == 4 &&  this.status == 200) {
 			var jObj = JSON.parse(this.responseText);
-			console.log(jObj.weather[0]);
-			console.log("testAjax");
+			populateWeatherConditions(jObj);
 
 		}
 	};
@@ -31,5 +27,28 @@ function callWeatherApi() {
 window.onload = callWeatherApi();
 
 function populateWeatherConditions(jObj){
+	//loop thru weather id to determine background image
+	var length = jObj.weather.length;
+	var id;
+	if(length <= 1) {
+		id = jObj.weather[0];
+	}
+	for(var i = 0; i < length; i++){
+		//keep the lowest id in the array & use for background
+		if(i == 0){
+			id = jObj.weather[0];
+		}
+		else {
+			if(id > jObj.weather[i]){
+				id = jObj.weather[i];
+			}
+		}
+	}
+	console.log(id);
+	
+	//store weather condition id's and current location in local storage
+	//grab associated background image and display it (with backup image color)
+	//display temp
+	//display city, and time
 
 }
