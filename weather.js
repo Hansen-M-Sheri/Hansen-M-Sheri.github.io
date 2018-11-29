e1fe0b2eec2ddf9cdd984072f320ac67c0092a2f
Sheri Hansen <10212888+Hansen-M-Sheri@users.noreply.github.com>
2018-11-28 23:13:26 -0700

    formatTime

--- weather.js
+++ weather.js
@@ -69,12 +69,13 @@ function storeWeatherData(jObj, id){
 
 function formatTime() {
     var d = new Date();
-    var m = d.getMinutes();
+    var hours = date.getHours();
+  	var minutes = date.getMinutes();
      var ampm = hours >= 12 ? 'pm' : 'am';
   	hours = hours % 12;
   	hours = hours ? hours : 12; // the hour '0' should be '12'
   	minutes = minutes < 10 ? '0'+minutes : minutes;
   	var strTime = hours + ':' + minutes + ' ' + ampm;
   	document.getElementById("timeAndDate").innerHTML =  strTime;
-    x.innerHTML = h + ":" + m + ":" + s;
+    
 }
