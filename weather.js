88a3dd2f7e8cf183916b7c79fbb8caf518b42eab
Sheri Hansen <10212888+Hansen-M-Sheri@users.noreply.github.com>
2018-11-28 20:16:31 -0700

    fix variable name

--- weather.js
+++ weather.js
@@ -10,14 +10,14 @@
 */
 function callWeatherApi() {
 	var apiCall = "api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=94c477428543f4d7a37d952a58632aab";
-	var xhttp = new XMLHttpRequest();
-	xhttp.onreadystatechange = function() {
+	var xmlhttp = new XMLHttpRequest();
+	xmlhttp.onreadystatechange = function() {
 		if (this.readyState == 4 &&  this.status == 200) {
 			var jObj = JSON.parse(this.responseText);
 			console.log(jObj);
 		}
 	};
-	xhttp.open("GET", apiCall, true);
+	xmlhttp.open("GET", apiCall, true);
 	xmlhttp.send()
 }
 window.onload = callWeatherApi();
