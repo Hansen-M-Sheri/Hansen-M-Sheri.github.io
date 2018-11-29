03034a572eb229badd74e736521ab283fa949bec
Sheri Hansen <10212888+Hansen-M-Sheri@users.noreply.github.com>
2018-11-28 20:47:53 -0700

    test api url

--- weather.js
+++ weather.js
@@ -9,16 +9,22 @@
 * use ajax to get the weather for the current zip code
 */
 function callWeatherApi() {
-	var apiCall = "api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=94c477428543f4d7a37d952a58632aab";
 	var xmlhttp = new XMLHttpRequest();
+	var city = "Boise,id&";
+	var api_key = "94c477428543f4d7a37d952a58632aab";
+	var appID = "APPID=" + api_key;
+	var url = "https://api.openweathermap.org/data/2.5/weather?";
+	url += city + appID;
+	console.log(url);
 	xmlhttp.onreadystatechange = function() {
 		if (this.readyState == 4 &&  this.status == 200) {
 			var jObj = JSON.parse(this.responseText);
 			console.log(jObj);
 			console.log("testAjax");
+
 		}
 	};
-	xmlhttp.open("GET", apiCall, true);
+	xmlhttp.open("GET", url, true);
 	xmlhttp.send()
 }
 window.onload = callWeatherApi();
