3fb528117521c79a27dc20024935476fe1a11768
Sheri Hansen <10212888+Hansen-M-Sheri@users.noreply.github.com>
2018-12-11 18:50:56 -0700

    refactoring code for js

---
+++ weatherApp.js
@@ -0,0 +1,36 @@
+/* Determine what screen to start on. Does zip exist?*/
+window.onload = checkNewUser();
+
+function checkNewUser(){
+	if(localStorage.getItem("userZip") === null){ //new visitor
+		console.log("zip not in localStorage");
+		//no zip - display welcome
+		document.getElementById("welcome").style.display = "none";
+	}
+	else{ //load ajax request
+		loadWeatherAjax(localStorage.getItem("userZip"), "us");
+	}
+	
+
+} 
+/*
+* Call Weather API
+* use ajax to get the weather for the current zip code
+*/
+function callWeatherApi(zip, country) {
+	var xmlhttp = new XMLHttpRequest();
+	var api_key = "94c477428543f4d7a37d952a58632aab";
+	var appID = "&APPID=" + api_key;
+	var url = "https://api.openweathermap.org/data/2.5/weather?zip=";
+	url += zip + "," + country + appID;
+	console.log(url);
+	xmlhttp.onreadystatechange = function() {
+		if (this.readyState == 4 &&  this.status == 200) {
+			localStorage.setItem("weatherObject", this.responseText);
+			//populateWeatherConditions();
+
+		}
+	};
+	xmlhttp.open("GET", url, true);
+	xmlhttp.send();
+}
