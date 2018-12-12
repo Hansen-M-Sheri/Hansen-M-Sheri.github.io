57fd5f60e38afb08c05aca2870ccd3a2401b3cb5
Sheri Hansen <10212888+Hansen-M-Sheri@users.noreply.github.com>
2018-12-11 20:00:13 -0700

    Add 5 day method

--- weatherApp.js
+++ weatherApp.js
@@ -33,6 +33,7 @@ function loadWeatherAjax(zip){
 * use ajax to get the weather for the current zip code
 */
 function callWeatherApi(zip, country) {
+	console.log("callWeatherApi called");
 	var xmlhttp = new XMLHttpRequest();
 	var api_key = "94c477428543f4d7a37d952a58632aab";
 	var appID = "&APPID=" + api_key;
@@ -42,11 +43,32 @@ function callWeatherApi(zip, country) {
 	xmlhttp.onreadystatechange = function() {
 		if (this.readyState == 4 &&  this.status == 200) {
 			localStorage.setItem("weatherObject", this.responseText);
+			console.log("weatherObject set in storage & is " + this.responseText);
 			//populateWeatherConditions();
 
 		}
 	};
 	xmlhttp.open("GET", url, true);
 	xmlhttp.send();
+	console.log("callWeatherApi finished");
 }
-
+/**
+* Get 5 day forecast
+*/
+function callFiveDayApi(zip, country){
+	//get the 5 day forecast
+	var xmlhttp = new XMLHttpRequest();
+	var api_key = "94c477428543f4d7a37d952a58632aab";
+	var appID = "&APPID=" + api_key;
+	var forecastUrl = "https://api.openweathermap.org/data/2.5/forecast?zip=";
+	forecastUrl += zip + "," + country + appID;
+	xmlhttp.onreadystatechange = function() {
+		if (this.readyState == 4 && this.status == 200) {
+			console.log(this.responseText);
+			localStorage.setItem("5dayWeatherObj", this.responseText);
+			populateFiveDay();
+		}
+	};
+	xmlhttp.open("GET", forecastUrl, true);
+	xmlhttp.send();
+}
